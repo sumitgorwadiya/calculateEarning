a78c7b2666111ea8a8311fee06fb6a0bf3ac5eb0
Sumit Gorwadiya <sumit.gorwadiya@empieretech.com>
2023-06-27 00:40:24 +0530

    clean code

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-import {StyleSheet} from 'react-native';
 import React from 'react';
 import HomeScreen from './src/Screens/HomeScreen';
-import {PaperProvider} from 'react-native-paper';
 
 const App = () => {
-  return (
-    <PaperProvider>
-      <HomeScreen />
-    </PaperProvider>
-  );
+  return <HomeScreen />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
